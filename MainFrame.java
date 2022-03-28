51542f1612ef01b10d2eae29c6f14b38aad4fb60
Andson2211 <102539754+Andson2211@users.noreply.github.com>
2022-03-28 17:52:37 +0200

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -14,62 +14,60 @@ import java.awt.Color;
 
 public class MainFrame extends JFrame {
 
-    private JPanel contentPane;
-    private JTextField vornameField
-    private JPanel contentPane; // Testkommentar? bbb
-    private JTextField vornameField;
-    private JTextField nachnameField;
-    private JTextField gebDatField;
-    private String[] newUsers = parseListToArray(Verwaltung.gymWueUser);
-    private JList<String> guiUserList = new JList<String>(newUsers);
+    private JPanel contentPane;         
+    private JTextField vornameField;  //Vornamefeld erzeugen
+    private JTextField nachnameField; //Nachnamefeld erzeugen
+    private JTextField gebDatField;   //Datumfeld erzeugen
+    private String[] newUsers = parseListToArray(Verwaltung.gymWueUser);  //Neue User wird erzeugt
+    private JList<String> guiUserList = new JList<String>(newUsers);      //List der Schüler des Gymnasiums
     private User selectedUser = null;
 
 
-    public FensterMeineErsteGUI() {
+    public MainFrame() {
 		//User[] t = (User[]) Verwaltung.gymWueUser.toArray();
-        setTitle("Meine erste GUI");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 150, 800, 270); // 400 + 260
+        setTitle("Meine erste GUI");                              //Erzeugung von GUI
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
+        setBounds(100, 150, 800, 270); // 400 + 260               //Eigenschaften (z.B. Größe des GUIs) 
         
          
-         contentPane = new JPanel();
+         contentPane = new JPanel();           //Eigenschaften des Panels (Design)
          contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
          setContentPane(contentPane);
          contentPane.setBackground(Color.decode("#247BA0"));
          contentPane.setLayout(null);
         
-         JLabel vornameLabel = new JLabel("Vorname: ");
+         JLabel vornameLabel = new JLabel("Vorname: ");           //Vorname als Überschrift erzeugen + Eigenschaften
          vornameLabel.setBounds(25, 15, 350, 20);
          vornameLabel.setForeground(Color.decode("#FAFAFF"));
          contentPane.add(vornameLabel);
          
         
-         vornameField = new JTextField();
+         vornameField = new JTextField();                         //Vornametextfeld erzeugen + Eigenschaften
          vornameField.setBounds(25, 35, 350, 25);
          contentPane.add(vornameField);
          
-         JLabel nachnameLabel = new JLabel("Nachname: ");
+         JLabel nachnameLabel = new JLabel("Nachname: ");        //Nachname als Überschrift erzeugen + Eigenschaften
          nachnameLabel.setBounds(25, 70, 350, 20);
-         nachnameLabel.setForeground(Color.decode("#FAFAFF"));
+         nachnameLabel.setForeground(Color.decode("#FAFAFF"));    
          contentPane.add(nachnameLabel);
          
         
-         nachnameField = new JTextField();
+         nachnameField = new JTextField();                       //Nachnametextfeld erzeugen + Eigenschaften
          nachnameField.setBounds(25, 95, 350, 25);
          contentPane.add(nachnameField);
     
-         JLabel gebDatLabel = new JLabel("Geburtsdatum: ");
+         JLabel gebDatLabel = new JLabel("Geburtsdatum: ");     //Geburtsdatum als Überschrift erzeugen + Eigenschaften
          gebDatLabel.setBounds(25, 125, 350, 20);
          gebDatLabel.setForeground(Color.decode("#FAFAFF"));
          contentPane.add(gebDatLabel);
          
         
-         gebDatField = new JTextField();
+         gebDatField = new JTextField();                         //Geburtsdatum als Überschrift erzeugen + Eigenschaften     
          gebDatField.setBounds(25, 150, 350, 25);
          contentPane.add(gebDatField);
          
          
-         guiUserList = new JList<>(newUsers);
+         guiUserList = new JList<>(newUsers);                    //Userlist wird erstellt + Eigenschaften
          guiUserList.addListSelectionListener(new ListSelectionListener() {
              @Override
              public void valueChanged(ListSelectionEvent e) {
@@ -100,7 +98,7 @@ public class MainFrame extends JFrame {
          //contentPane.add(guiUserList);
          
 
-         JButton submitButton = new JButton("add");
+         JButton submitButton = new JButton("add");                       //"Add"button wird hergestellt + Eigenschaften
          submitButton.setBackground(Color.decode("#3C4644"));
          submitButton.setForeground(Color.decode("#000000"));
          submitButton.addActionListener(new ActionListener() {
@@ -115,7 +113,7 @@ public class MainFrame extends JFrame {
         submitButton.setBounds(260, 190, 115, 25);
         contentPane.add(submitButton);
 
-        JButton editButton = new JButton("edit");
+        JButton editButton = new JButton("edit");                       //"Edit"button wird erzeugt + Eigenschaften
         editButton.setBackground(Color.decode("#3C4644"));
         editButton.setForeground(Color.decode("#000000"));
         editButton.addActionListener(new ActionListener() {
@@ -137,10 +135,10 @@ public class MainFrame extends JFrame {
                 }
             }
         });
-        editButton.setBounds(140, 190, 120, 25);
+        editButton.setBounds(140, 190, 120, 25);                 //Eigenschaften des Buttons (z.B. Größe)
         contentPane.add(editButton);
 
-        JButton deleteButton = new JButton("delete");
+        JButton deleteButton = new JButton("delete");           //"Delete"button wird festgelegt + Eigenschaften 
         deleteButton.setBackground(Color.decode("#3C4644"));
         deleteButton.setForeground(Color.decode("#000000"));
         deleteButton.addActionListener(new ActionListener() {
@@ -168,7 +166,7 @@ public class MainFrame extends JFrame {
     }
     
     
-    private String[] arrayToStringArray(User[] list) {
+    private String[] arrayToStringArray(User[] list) {                     //Array wird zu String 
         String[] arr = new String[list.length];
         for (int i = 0; i < list.length; i++) {
             arr[i] = list[i].username;
@@ -177,8 +175,8 @@ public class MainFrame extends JFrame {
         return arr;
     }
 
-    private String[] parseListToArray(List<User> list) {
-        String[] stringList = new String[list.size()];
+    private String[] parseListToArray(List<User> list) {                    //Liste als Array festlegen
+//        String[] stringList = new String[list.size()];
         System.out.println(list.size());
         list.toFirst();
         for(int i=0; list.hasAccess(); i++) {
@@ -188,7 +186,7 @@ public class MainFrame extends JFrame {
         return stringList;
     }
 
-    private User getUserByUsername(List<User> list, String username) {
+    private User getUserByUsername(List<User> list, String username) {     //User wird aus der Liste aausgewählt
         list.toFirst();
         while (list.hasAccess()) {
             if (list.getContent().getUsername() == username) {
